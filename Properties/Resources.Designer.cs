8d7102b99dee6c9e89e805c8ef6909dbd5604f75
iminet <kincsesimi83@gmail.com>
2023-09-29 13:29:28 +0200

    Added icon and NuGet package info

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,9 +63,9 @@ namespace ImgBBApi.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap iminetsoft {
+        internal static System.Drawing.Bitmap imgbb {
             get {
-                object obj = ResourceManager.GetObject("iminetsoft", resourceCulture);
+                object obj = ResourceManager.GetObject("imgbb", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
