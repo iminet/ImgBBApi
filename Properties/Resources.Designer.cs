3ad6342b07990ec1efe64d69676012da84d2300f
iminet <kincsesimi83@gmail.com>
2023-09-29 13:36:21 +0200

    Added icon and NuGet package info

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace ImgBBApi.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap imgbb_invert {
+            get {
+                object obj = ResourceManager.GetObject("imgbb-invert", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
